56347b76ed8450b05c0c4b146be458e61e883497
Tony Josi <tonyjosi@amazon.com>
2025-05-08 13:47:19 +0530

    Remove mbedtls debug macro (#41)
    * Remove mbedtls debug macro
    
    * Add DEBUG flag to CMake when build with Debug build type
    
    * Add DEBUG flag to CMake when build with Debug build type
    
    * Code review suggestions.
    
    ---------
    
    Co-authored-by: ActoryOu <ousc@amazon.com>

--- configs/mbedtls/mbedtls_custom_config.h
+++ configs/mbedtls/mbedtls_custom_config.h
@@ -21,4 +21,10 @@
 
 #include "mbedtls/config.h"
 
+#if DEBUG != 0
+    #define MBEDTLS_DEBUG_C
+#else /* DEBUG != 0 */
+    #undef MBEDTLS_DEBUG_C
+#endif /* DEBUG != 0 */
+
 #endif /* MBEDTLS_CUSTOM_CONFIG_H */
