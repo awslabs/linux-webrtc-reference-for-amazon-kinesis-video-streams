2c89e0e463a5df80ecda4bca87e6b6709b83442d
Tiya Jain <tiyajain2002@gmail.com>
2025-05-16 10:05:58 +0000

    Configure LWS Logs

---
+++ examples/networking/networking_utils/configure_lws_logging.c
@@ -0,0 +1,44 @@
+#include "../networking.h"
+#include "logging.h"
+
+NetworkingResult_t Networking_ConfigureLwsLogging( uint32_t logLevel )
+{
+    NetworkingResult_t ret = NETWORKING_RESULT_OK;
+    int lws_levels = 0;
+    LogInfo(("Log level: %d", logLevel));
+    /* Map application log levels to libwebsockets log levels */
+    if( logLevel == LOG_NONE )
+    {
+        lws_levels = 0;
+    }
+
+    if( logLevel >= LOG_ERROR )
+    {
+        lws_levels |= LLL_ERR;
+    }
+
+    if( logLevel >= LOG_WARN )
+    {
+        lws_levels |= LLL_WARN;
+    }
+
+    if( logLevel >= LOG_INFO )
+    {
+        lws_levels |= LLL_NOTICE;
+    }
+
+    if( logLevel >= LOG_DEBUG )
+    {
+        lws_levels |= LLL_INFO;
+    }
+
+    if( logLevel >= LOG_VERBOSE )
+    {
+        lws_levels |= LLL_DEBUG;
+    }
+
+    /* Configure libwebsockets with the mapped log levels */
+    lws_set_log_level( lws_levels, NULL );
+
+    return ret;
+}
