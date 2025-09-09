9ef707ca0dad4b5e2edb8239845e411a2f3fdb86
ActoryOu <ousc@amazon.com>
2025-09-09 08:59:48 +0000

    Refactor app media source part.

--- examples/app_media_source/app_media_source.c
+++ examples/app_media_source/app_media_source.c
@@ -81,36 +81,40 @@ static void * VideoTx_Task( void * pParameter )
             #ifndef ENABLE_STREAMING_LOOPBACK
                 if( pVideoContext->numReadyPeer != 0 )
                 {
-                    #if USE_VIDEO_CODEC_H265
-                    {
-                        pVideoContext->fileIndex = pVideoContext->fileIndex % NUMBER_OF_H265_FRAME_SAMPLE_FILES + 1;
-                        ( void ) snprintf( filePath, MAX_PATH_LEN, "./examples/app_media_source/samples/h265SampleFrames/frame-%04d.h265", pVideoContext->fileIndex );
-                    }
-                    #else
-                    {
-                        pVideoContext->fileIndex = pVideoContext->fileIndex % NUMBER_OF_H264_FRAME_SAMPLE_FILES + 1;
-                        ( void ) snprintf( filePath, MAX_PATH_LEN, "./examples/app_media_source/samples/h264SampleFrames/frame-%04d.h264", pVideoContext->fileIndex );
-                    }
-                    #endif
+                    do {
+                        /* Open file */
+                        #if USE_VIDEO_CODEC_H265
+                        {
+                            pVideoContext->fileIndex = pVideoContext->fileIndex % NUMBER_OF_H265_FRAME_SAMPLE_FILES + 1;
+                            ( void ) snprintf( filePath, MAX_PATH_LEN, "./examples/app_media_source/samples/h265SampleFrames/frame-%04d.h265", pVideoContext->fileIndex );
+                        }
+                        #else
+                        {
+                            pVideoContext->fileIndex = pVideoContext->fileIndex % NUMBER_OF_H264_FRAME_SAMPLE_FILES + 1;
+                            ( void ) snprintf( filePath, MAX_PATH_LEN, "./examples/app_media_source/samples/h264SampleFrames/frame-%04d.h264", pVideoContext->fileIndex );
+                        }
+                        #endif
 
-                    fp = fopen( filePath, "rb" );
+                        fp = fopen( filePath, "rb" );
+                        if( fp == NULL )
+                        {
+                            LogError( ( "Failed to open %s. Error: %s", filePath, strerror( errno ) ) );
+                            break;
+                        }
 
-                    if( fp == NULL )
-                    {
-                        LogError( ( "Failed to open %s. Error: %s", filePath, strerror( errno ) ) );
-                    }
-                    else
-                    {
+                        /* Get file size */
                         fseek( fp, 0, SEEK_END );
                         frameLength = ftell( fp );
 
                         if( frameLength < 0 )
                         {
                             LogError( ( "ftell failed on file %s", filePath ) );
-                            fclose( fp );
-                            continue;
+                            break;
                         }
-                        else if( frameLength > allocatedBufferLength )
+
+                        /* Check buffer size */
+                        if( frame.pData == NULL ||
+                            frameLength > allocatedBufferLength )
                         {
                             if( frame.pData != NULL )
                             {
@@ -123,10 +127,11 @@ static void * VideoTx_Task( void * pParameter )
                             {
                                 LogError( ( "malloc failed!" ) );
                                 allocatedBufferLength = 0;
-                                fclose( fp );
-                                continue;
+                                break;
                             }
                         }
+
+                        /* Read */
                         frame.size = frameLength;
                         frame.timestampUs += SAMPLE_VIDEO_FRAME_DURATION_IN_US;
                         frame.trackKind = TRANSCEIVER_TRACK_KIND_VIDEO;
@@ -144,7 +149,11 @@ static void * VideoTx_Task( void * pParameter )
                         {
                             LogError( ( "VideoTx_Task: fread failed!" ) );
                         }
+                    } while( 0 );
 
+                    /* Close file */
+                    if( fp != NULL )
+                    {
                         fclose( fp );
                     }
                 }
@@ -181,27 +190,31 @@ static void * AudioTx_Task( void * pParameter )
             #ifndef ENABLE_STREAMING_LOOPBACK
                 if( pAudioContext->numReadyPeer != 0 )
                 {
-                    pAudioContext->fileIndex = pAudioContext->fileIndex % NUMBER_OF_OPUS_FRAME_SAMPLE_FILES + 1;
-                    ( void ) snprintf( filePath, MAX_PATH_LEN, "./examples/app_media_source/samples/opusSampleFrames/sample-%03d.opus", pAudioContext->fileIndex );
+                    do {
+                        /* Open file */
+                        pAudioContext->fileIndex = pAudioContext->fileIndex % NUMBER_OF_OPUS_FRAME_SAMPLE_FILES + 1;
+                        ( void ) snprintf( filePath, MAX_PATH_LEN, "./examples/app_media_source/samples/opusSampleFrames/sample-%03d.opus", pAudioContext->fileIndex );
 
-                    fp = fopen( filePath, "rb" );
+                        fp = fopen( filePath, "rb" );
+                        if( fp == NULL )
+                        {
+                            LogError( ( "Failed to open %s. Error: %s", filePath, strerror( errno ) ) );
+                            break;
+                        }
 
-                    if( fp == NULL )
-                    {
-                        LogError( ( "Failed to open %s.", filePath ) );
-                    }
-                    else
-                    {
+                        /* Get file size */
                         fseek( fp, 0, SEEK_END );
                         frameLength = ftell( fp );
 
                         if( frameLength < 0 )
                         {
                             LogError( ( "ftell failed on file %s", filePath ) );
-                            fclose( fp );
-                            continue;
+                            break;
                         }
-                        else if( frameLength > allocatedBufferLength )
+
+                        /* Check buffer size */
+                        if( frame.pData == NULL ||
+                            frameLength > allocatedBufferLength )
                         {
                             if( frame.pData != NULL )
                             {
@@ -214,13 +227,14 @@ static void * AudioTx_Task( void * pParameter )
                             {
                                 LogError( ( "malloc failed!" ) );
                                 allocatedBufferLength = 0;
-                                fclose( fp );
-                                continue;
+                                break;
                             }
                         }
+
+                        /* Read */
                         frame.size = frameLength;
-                        frame.timestampUs += SAMPLE_AUDIO_FRAME_DURATION_IN_US;
-                        frame.trackKind = TRANSCEIVER_TRACK_KIND_AUDIO;
+                        frame.timestampUs += SAMPLE_VIDEO_FRAME_DURATION_IN_US;
+                        frame.trackKind = TRANSCEIVER_TRACK_KIND_VIDEO;
 
                         fseek( fp, 0, SEEK_SET );
                         if( fread( frame.pData, frameLength, 1, fp ) == 1 )
@@ -233,9 +247,13 @@ static void * AudioTx_Task( void * pParameter )
                         }
                         else
                         {
-                            LogError( ( "AudioTx_Task: fread failed!" ) );
+                            LogError( ( "VideoTx_Task: fread failed!" ) );
                         }
+                    } while( 0 );
 
+                    /* Close file */
+                    if( fp != NULL )
+                    {
                         fclose( fp );
                     }
                 }
